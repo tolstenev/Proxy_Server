289a582f791682f0e17e579dc39fe6b2739bac7b
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-09-03 23:46:06 +0300

    Save commit.

--- src/client.cpp
+++ src/client.cpp
@@ -6,165 +6,110 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
-void saveLoginDataToFile() {
-  std::string host, port, database, user, password;
-
-  // Запрос данных у пользователя
-  std::cout << "Введите хост: ";
-  std::getline(std::cin, host);
-
-  std::cout << "Введите порт: ";
-  std::getline(std::cin, port);
-
-  std::cout << "Введите имя базы данных: ";
-  std::getline(std::cin, database);
-
-  std::cout << "Введите имя пользователя: ";
-  std::getline(std::cin, user);
-
-  std::cout << "Введите пароль: ";
-  std::getline(std::cin, password);
-
-  // Открытие файла для записи
-  std::ofstream file("login_data.txt");
-  if (!file.is_open()) {
-    std::cout << "Не удалось открыть файл для записи." << std::endl;
-  } else {
-    // Запись данных в файл
-    file << "host," << host << std::endl;
-    file << "port," << port << std::endl;
-    file << "database," << database << std::endl;
-    file << "user," << user << std::endl;
-    file << "password," << password << std::endl;
-
-    // Закрытие файла
-    file.close();
-
-    std::cout << "Данные успешно сохранены в файл login_data.txt." << std::endl;
-  }
-}
-
-void sendFileToServer(const std::string& filename, const std::string& serverIP, int serverPort) {
-  // Открытие файла для чтения
-  std::ifstream file(filename, std::ios::binary);
-  if (!file.is_open()) {
-    std::cout << "Failed to open file" << std::endl;
-    return;
-  }
-
-  // Получение размера файла
-  file.seekg(0, std::ios::end);
-  std::streampos fileSize = file.tellg();
-  file.seekg(0, std::ios::beg);
-
-  // Чтение содержимого файла в буфер
-  std::vector<char> fileContent(fileSize);
-  file.read(fileContent.data(), fileSize);
-
-  // Создание сокета
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    std::cout << "Failed to create socket" << std::endl;
-    return;
-  }
-
-  // Установка адреса сервера и порта
-  sockaddr_in serverAddr{};
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(serverPort);
-  if (inet_pton(AF_INET, serverIP.c_str(), &(serverAddr.sin_addr)) <= 0) {
-    std::cout << "Invalid address or address not supported" << std::endl;
-    close(sockfd);
-    return;
-  }
-
-  // Установка соединения с сервером
-  if (connect(sockfd, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr)) < 0) {
-    std::cout << "Connection failed" << std::endl;
-    close(sockfd);
-    return;
-  }
-
-  // Отправка размера файла на сервер
-  if (send(sockfd, &fileSize, sizeof(fileSize), 0) < 0) {
-    std::cout << "Failed to send file size" << std::endl;
-    close(sockfd);
-    return;
-  }
-
-  // Отправка содержимого файла на сервер
-  if (send(sockfd, fileContent.data(), fileSize, 0) < 0) {
-    std::cout << "Failed to send file content" << std::endl;
-    close(sockfd);
-    return;
-  }
-
-  // Закрытие соединения и освобождение ресурсов
-  close(sockfd);
-}
+enum Error_codes {
+  OK, ERROR
+};
 
 int main() {
+  int errcode = OK;
+
   // Создаем сокет
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     std::cout << "Failed to create socket" << std::endl;
-    return 1;
+    errcode = ERROR;
+  } else {
+
+    // Устанавливаем адрес сервера и порт
+    std::string serverIP = "127.0.0.1";
+    int serverPort = 8081;
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+    if (inet_pton(AF_INET, serverIP.c_str(), &(serverAddr.sin_addr)) <= 0) {
+      std::cout << "Invalid address or address not supported" << std::endl;
+      close(sockfd);
+      errcode = ERROR;
+    } else {
+
+      // Устанавливаем соединение с сервером
+      if (connect(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+        std::cout << "Connection failed" << std::endl;
+        close(sockfd);
+        errcode = ERROR;
+      } else {
+        bool loginDataIsSet = false;
+        std::string loginData;
+
+
+        if (!loginDataIsSet) {
+          // Запрос данных для входа на SQL сервер у пользователя
+          std::string host, port, dbname, user, password;
+
+          std::cout << "Введите хост: ";
+          std::cin >> host;
+          std::cout << "Введите порт: ";
+          std::cin >> port;
+          std::cout << "Введите имя базы данных: ";
+          std::cin >> dbname;
+          std::cout << "Введите имя пользователя: ";
+          std::cin >> user;
+          std::cout << "Введите пароль: ";
+          std::cin >> password;
+
+          loginData += "host," + host;
+          loginData += ",port," + port;
+          loginData += ",dbname," + dbname;
+          loginData += ",user," + user;
+          loginData += ",password," + password;
+
+          loginDataIsSet = true;
+        }
+        std::string request = loginData;
+
+        // Формируем SQL-запрос на сервер
+        std::string query;
+        std::cout << "Введите SQL-запрос: ";
+        std::getline(std::cin, query);
+        if (!query.empty()) {
+          query.pop_back();
+        }
+//        std::cin >> query;
+//        request += ",query," + query;
+
+        std::cout << request << std::endl;
+
+        if (send(sockfd, request.c_str(), request.length(), 0) < 0) {
+          std::cout << "Failed to send request" << std::endl;
+          close(sockfd);
+          errcode = ERROR;
+        } else {
+
+          // Получаем ответ от сервера
+          char buffer[4096];
+          memset(buffer, 0, sizeof(buffer));
+          std::string response;
+
+          while (true) {
+            int bytesRead = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+            if (bytesRead <= 0)
+              break;
+            response += buffer;
+            memset(buffer, 0, sizeof(buffer));
+          }
+
+          // Выводим ответ от сервера
+          std::cout << "Response from server:" << std::endl;
+          std::cout << response << std::endl;
+
+          // Закрываем соединение и освобождаем ресурсы
+          close(sockfd);
+        }
+      }
+    }
   }
-
-  std::string filename = "login_data.txt";
-  sendFileToServer(filename, serverIP, serverPort);
-
-  // Устанавливаем адрес сервера и порт
-  std::string serverIP = "127.0.0.1";
-  int serverPort = 8080;
-
-  sockaddr_in serverAddr{};
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(serverPort);
-  if (inet_pton(AF_INET, serverIP.c_str(), &(serverAddr.sin_addr)) <= 0) {
-    std::cout << "Invalid address or address not supported" << std::endl;
-    close(sockfd);
-    return 1;
-  }
-
-  // Устанавливаем соединение с сервером
-  if (connect(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-    std::cout << "Connection failed" << std::endl;
-    close(sockfd);
-    return 1;
-  }
-
-  // Отправляем SQL-запрос на сервер
-  std::string sqlQuery = "SELECT * FROM peers";
-  std::string request = sqlQuery + ";";
-  if (send(sockfd, request.c_str(), request.length(), 0) < 0) {
-    std::cout << "Failed to send SQL query" << std::endl;
-    close(sockfd);
-    return 1;
-  }
-
-  // Получаем ответ от сервера
-  char buffer[4096];
-  memset(buffer, 0, sizeof(buffer));
-  std::string response;
-
-  while (true) {
-    int bytesRead = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead <= 0)
-      break;
-    response += buffer;
-    memset(buffer, 0, sizeof(buffer));
-  }
-
-  // Выводим ответ от сервера
-  std::cout << "Response from server:" << std::endl;
-  std::cout << response << std::endl;
-
-
-
-  // Закрываем соединение и освобождаем ресурсы
-  close(sockfd);
-
-  return 0;
+  return errcode;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -5,8 +5,10 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <libpq-fe.h>
+#include <unordered_map>
+#include <sstream>
 
-#define SERVER_PORT 8080
+#define SERVER_PORT 8081
 #define BUFF_SIZE 4096
 #define SOCKET_ERROR -1
 
@@ -57,80 +59,114 @@ int main() {
           int clientSocket = accept(proxySocket, reinterpret_cast<sockaddr *>(&clientAddress), &clientAddressLength);
           if (clientSocket == SOCKET_ERROR) {
             std::cerr << "Failed to accept client connection" << std::endl;
+            ++clientId;
             continue;
           }
 
           std::cout << "Accepted new client connection #" << clientId << std::endl;
+          // Обработка клиентского запроса
+          char buffer[BUFF_SIZE];
+          ssize_t bytesRead;
 
-          db_host = ;
-          db_port = ;
-          db_dbname = ;
-          db_user = ;
-          db_password = ;
+          while ((bytesRead = recv(clientSocket, buffer, BUFF_SIZE - 1, 0)) > 0) {
+            buffer[bytesRead] = '\0';
+          }
+          std::cout << std::endl << buffer << std::endl;
 
+          std::string buff = buffer;
+          std::istringstream iss(buff);
+          std::unordered_map<std::string, std::string> db;
 
+          std::string key;
+          std::string value;
+          while (std::getline(iss, key, ',')) {
+            std::getline(iss, value, ','); // Считываем следующую подстроку после запятой
+            db[key] = value;
+          }
 
+          std::cout << "db_host " << db["host"] << std::endl;
+          std::cout << "db_port " << db["port"] << std::endl;
+          std::cout << "db_name " << db["dbname"] << std::endl;
+          std::cout << "db_username " << db["user"] << std::endl;
+          std::cout << "db_password " << db["password"] << std::endl;
+          std::cout << "db_query " << db["query"] << std::endl;
+
+          // Создание строки с данными для входа на сервер PostgreSQL
+          std::string connection_data;
+          connection_data += "host=" + db["host"];
+          connection_data += " port=" + db["port"];
+          connection_data += " dbname=" + db["dbname"];
+          connection_data += " user=" + db["user"];
+          connection_data += " password=" + db["password"];
 
           // Создание соединения с сервером PostgreSQL
-          PGconn *pgConnection = PQconnectdb("host=localhost port=5432 dbname=info_21 user=yonnarge password=1");
+//          PGconn *pgConnection = PQconnectdb("host=localhost port=5432 dbname=info_21 user=yonnarge password=1");
+          PGconn *pgConnection = PQconnectdb(connection_data.c_str());
           if (PQstatus(pgConnection) != CONNECTION_OK) {
             std::cerr << "Failed to connect to PostgreSQL server: " << PQerrorMessage(pgConnection) << std::endl;
+
+/* ...Отправить клиенту сообщение, с описанием ошибки...*/
+
             close(clientSocket);
+            ++clientId;
             continue;
           }
 
           // Обработка клиентского запроса
-          char buffer[BUFF_SIZE];
-          ssize_t bytesRead;
-          std::string query;
+//          char buffer[BUFF_SIZE];
+//          ssize_t bytesRead;
+//          std::string query;
+//
+//          while ((bytesRead = recv(clientSocket, buffer, BUFF_SIZE - 1, 0)) > 0) {
+//            buffer[bytesRead] = '\0';
+//            query += buffer;
 
-          while ((bytesRead = recv(clientSocket, buffer, BUFF_SIZE - 1, 0)) > 0) {
-            buffer[bytesRead] = '\0';
-            query += buffer;
+          std::string query = db["query"];
 
-            // Проверка наличия завершающего символа запроса
-            if (query.find(';') != std::string::npos) {
-              // Отправка SQL-запроса на сервер PostgreSQL
-              PGresult *pgResult = PQexec(pgConnection, query.c_str());
-              if (PQresultStatus(pgResult) == PGRES_COMMAND_OK || PQresultStatus(pgResult) == PGRES_TUPLES_OK) {
-                // Логирование SQL-запроса
-                logQuery(query);
+          // Проверка наличия завершающего символа запроса
+          if (query.find(';') != std::string::npos) {
+            // Отправка SQL-запроса на сервер PostgreSQL
+            PGresult *pgResult = PQexec(pgConnection, query.c_str());
+            if (PQresultStatus(pgResult) == PGRES_COMMAND_OK || PQresultStatus(pgResult) == PGRES_TUPLES_OK) {
+              // Логирование SQL-запроса
+              logQuery(query);
 
-                // Пример формирования ответа на основе результата SQL-запроса
-                std::string response = "Result:\n";
-                int row_count = PQntuples(pgResult);
-                int col_count = PQnfields(pgResult);
+              // Пример формирования ответа на основе результата SQL-запроса
+              std::string response = "Result:\n";
+              int row_count = PQntuples(pgResult);
+              int col_count = PQnfields(pgResult);
 
-                for (int row = 0; row < row_count; ++row) {
-                  for (int col = 0; col < col_count; ++col) {
-                    response += PQgetvalue(pgResult, row, col);
-                    response += "\t";
-                  }
-                  response += "\n";
+              for (int row = 0; row < row_count; ++row) {
+                for (int col = 0; col < col_count; ++col) {
+                  response += PQgetvalue(pgResult, row, col);
+                  response += "\t";
                 }
-                // Отправка ответа клиенту
-                if (send(clientSocket, response.c_str(), response.length(), 0) == SOCKET_ERROR) {
-                  std::cerr << "Failed to send response to client" << std::endl;
-                  break;
-                }
-              } else {
-                std::cerr << "Failed to execute SQL query: " << PQresultErrorMessage(pgResult) << std::endl;
+                response += "\n";
               }
-              // Очистка результата запроса
-              PQclear(pgResult);
-              // Очистка буфера запроса
-              query.clear();
+              // Отправка ответа клиенту
+              if (send(clientSocket, response.c_str(), response.length(), 0) == SOCKET_ERROR) {
+                std::cerr << "Failed to send response to client" << std::endl;
+                break;
+              }
+            } else {
+              std::cerr << "Failed to execute SQL query: " << PQresultErrorMessage(pgResult) << std::endl;
             }
-            // Закрытие клиентского соединения
-            close(clientSocket);
-            std::cout << "Close client connection #" << clientId << std::endl;
+            // Очистка результата запроса
+            PQclear(pgResult);
+            // Очистка буфера запроса
+            query.clear();
           }
-          // Закрытие соединения с сервером PostgreSQL
-          PQfinish(pgConnection);
+          // Закрытие клиентского соединения
+          close(clientSocket);
+          std::cout << "Close client connection #" << clientId << std::endl;
+
+          ++clientId;
+        // Закрытие соединения с сервером PostgreSQL
+        PQfinish(pgConnection);
         }
-        // Закрытие прокси-сервера
-        close(proxySocket);
       }
+      // Закрытие прокси-сервера
+      close(proxySocket);
     }
   }
   return errcode;
